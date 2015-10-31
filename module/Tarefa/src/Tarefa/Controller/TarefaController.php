5f89533767b52046dea7fa5814e1ee35f3290282
Lucas Burg <lucasburgmota@gmail.com>
2015-10-30 22:06:39 -0200

    Não está encontrando as views

---
+++ module/Tarefa/src/Tarefa/Controller/TarefaController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Tarefa\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class TarefaController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return array();
+    }
+}
