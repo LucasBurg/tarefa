a5c964b687516b21b5b2e1227ba44b87248bc7a2
Lucas Burg <lucasburgmota@gmail.com>
2015-11-12 00:35:21 -0200

    Tirado o redirecionamento.

--- module/Tarefa/src/Tarefa/Controller/IndexController.php
+++ module/Tarefa/src/Tarefa/Controller/IndexController.php
@@ -11,7 +11,7 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-	return $this->redirect()->toRoute('tarefa');
+	//return $this->redirect()->toRoute('tarefa');
     }
     
 }
